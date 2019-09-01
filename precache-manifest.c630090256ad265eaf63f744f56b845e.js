695bfb806c439d5aa02e58ddbb0eb6fd12515519
Денис Пермяков <permyakov.d.a.92@mail.ru>
2019-09-02 02:52:15 +0500

    Updates

---
+++ precache-manifest.c630090256ad265eaf63f744f56b845e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ab1c92298dd75deb8fada591eba840",
+    "url": "/byndyusofttaskapp/index.html"
+  },
+  {
+    "revision": "30f9cfeae1a4c0528fb7",
+    "url": "/byndyusofttaskapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ea6b030806565294a4ca",
+    "url": "/byndyusofttaskapp/static/js/2.8e918357.chunk.js"
+  },
+  {
+    "revision": "30f9cfeae1a4c0528fb7",
+    "url": "/byndyusofttaskapp/static/js/main.3cba9d93.chunk.js"
+  },
+  {
+    "revision": "c6044eb83dc2d02c4f11",
+    "url": "/byndyusofttaskapp/static/js/runtime~main.c81e98c3.js"
+  }
+]);
